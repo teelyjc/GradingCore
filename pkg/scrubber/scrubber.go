54a2a41ab1019b8537777e01f23fa4801e58f39e
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-06-08 17:53:46 +0700

    feat: trim whitespace from data

--- pkg/scrubber/scrubber.go
+++ pkg/scrubber/scrubber.go
@@ -4,16 +4,20 @@ import (
 	"strings"
 )
 
-func Scrub(data []byte) []byte {
-	return []byte(ScrubString(string(data)))
+func Scrub(data []byte, autoTrimEnabled bool) []byte {
+	return []byte(ScrubString(string(data), autoTrimEnabled))
 }
 
-func ScrubString(data string) string {
+func ScrubString(data string, autoTrimEnabled bool) string {
 	split := strings.Split(data, "\n")
 	splitlen := len(split)
 	for i := 0; i < splitlen; i++ {
 		split[i] = strings.TrimSuffix(split[i], "\r")
+		if autoTrimEnabled {
+			split[i] = strings.TrimSpace(split[i])
+		}
 	}
+
 	if split[splitlen-1] == "" {
 		split = split[0 : splitlen-1]
 	}
