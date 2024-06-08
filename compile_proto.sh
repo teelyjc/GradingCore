970c732f7eddeb8a21c80075b5b2abb0630a3ef6
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-06-08 17:51:17 +0700

    docs: add protoc's url for setting up protoc for other device

--- compile_proto.sh
+++ compile_proto.sh
@@ -1,1 +1,2 @@
- protoc ./rin.proto --go_out=./pkg/ --go-grpc_out=./pkg/
+# https://protobuf.dev/getting-started/gotutorial/#compiling-protocol-buffers
+protoc ./rin.proto --go_out=./pkg/ --go-grpc_out=./pkg/
